94552c381d2ce0dca67acf288ed9aa59f5858fcc
Isaac Batista <isaacbatst@gmail.com>
2022-09-06 21:33:55 -0300

    add user tests

---
+++ src/entities/User.test.ts
@@ -0,0 +1,52 @@
+import { User } from "./User"
+
+describe('User', () => {
+  describe('Given an invalid email', () => {
+    it('should throw INVALID_EMAIL error', () => {
+      expect(() => {
+        new User({
+          email: 'invalid-email',
+          name: 'any-name',
+          password: 'any-password'
+        })
+      }).toThrow('INVALID_EMAIL')
+    })
+  })
+
+  describe('Given an invalid password', () => {
+    // check readme for valid password
+    it('should throw INVALID_PASSWORD error', () => {
+      expect(() => {
+        new User({
+          email: 'any@email.com',
+          name: 'any-name',
+          password: 'invalid-password'
+        })
+      }).toThrow('INVALID_PASSWORD')
+    })
+  })
+
+  describe('Given a bellow min length name', () => {
+    it('should throw NAME_LENGTH_BELLOW_MIN error', () => {
+      expect(() => {
+        new User({
+          email: 'any@email.com',
+          name: 'a',
+          password: '1234567a'
+        })
+      }).toThrow('NAME_LENGTH_BELLOW_MIN')
+    })
+  })
+
+  describe('Given a above max length name', () => {
+    it('should throw NAME_LENGTH_ABOVE_MAX error', () => {
+      expect(() => {
+        new User({
+          email: 'any@email.com',
+          name: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+          password: '1234567a'
+        })
+      }).toThrow('NAME_LENGTH_ABOVE_MAX')
+    })
+  })
+})
